afa04781a67c0a02907dce61168f1e8d22a93107
Diablodesu <diablodevgay@protonmail.com>
2022-07-15 02:20:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WhichNumberIsBigger
+ A tiny python program for people like me who can't read numbers very well to tell you which number is bigger
---
+++ whichnumberisbigger_v1.0.py
@@ -0,0 +1,11 @@
+# Input numbers 
+x = input('Enter first number: ')
+y = input('Enter second number: ')
+
+
+# Calculation
+if int(x) > int(y):
+    print("Your first number is greater than your second number.")
+
+if int(x) < int(y):
+    print("Your second number is greater than your first number.")
